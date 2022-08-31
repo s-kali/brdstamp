2af58b1458eb4fdc295712a6696121071a7961c3
Süha KÜÇÜKALİ <suhakucukali@marun.edu.tr>
2022-08-31 02:53:04 -0400

    socker&bind structure

--- receiver.c
+++ receiver.c
@@ -25,4 +25,12 @@ int main(int argc, char *argv[]){
 
   broadcastPort = atoi(argv[1]);
 
+  if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+    throwError("socket() failed");
+
+  memset(&broadcastAddr, 0, sizeof(broadcastAddr));
+  broadcastAddr.sin_family = AF_INET;
+  broadcastAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  broadcastAddr.sin_port = htons(broadcastPort);
+
 }
