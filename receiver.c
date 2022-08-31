47dbb416da672195107e0ceff38858ac9b91b547
Süha KÜÇÜKALİ <suhakucukali@marun.edu.tr>
2022-08-31 04:10:16 -0400

    receive sender as host order

--- receiver.c
+++ receiver.c
@@ -13,6 +13,10 @@ void throwError(char *errorString){
 int main(int argc, char *argv[]){
 
   int sock;
+
+  struct sockaddr_in msgSender;
+  socklen_t msgLen = sizeof(msgSender);
+
   struct sockaddr_in broadcastAddr;
   unsigned short broadcastPort;
   char recvString[20];
@@ -33,14 +37,17 @@ int main(int argc, char *argv[]){
   broadcastAddr.sin_addr.s_addr = htonl(INADDR_ANY);
   broadcastAddr.sin_port = htons(broadcastPort);
 
-  if(bind(sock, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr)) < 0)
+  if(bind(sock, (struct sockaddr *)&broadcastAddr,
+	 sizeof(broadcastAddr)) < 0)
     throwError("bind() failed");
 
-  if((recvStringLen = recvfrom(sock, recvString, 20, 0, NULL, 0)) < 0)
+  if((recvStringLen = recvfrom(sock, recvString, 20,
+	 0, (struct sockaddr*)&msgSender, &msgLen)) < 0)
     throwError("recvfrom() failed");
 
   recvString[recvStringLen] = '\0';
-  printf("Recieved: %s\n", recvString);
+  printf("Recieved: %s from %d\n",
+	recvString, ntohl(msgSender.sin_addr.s_addr));
 
   close(sock);
   exit(0);
