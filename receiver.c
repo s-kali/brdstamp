823ca92b5c72b34047211d4768748c8bb7cdaef4
Süha KÜÇÜKALİ <suhakucukali@marun.edu.tr>
2022-08-31 06:36:32 -0400

    print ip & recv loop

--- receiver.c
+++ receiver.c
@@ -10,6 +10,16 @@ void throwError(char *errorString){
   exit(1);
 }
 
+void print_ip(unsigned int ip)
+{
+    unsigned char bytes[4];
+    bytes[0] = ip & 0xFF;
+    bytes[1] = (ip >> 8) & 0xFF;
+    bytes[2] = (ip >> 16) & 0xFF;
+    bytes[3] = (ip >> 24) & 0xFF;
+    printf("%d.%d.%d.%d\n", bytes[3], bytes[2], bytes[1], bytes[0]);
+}
+
 int main(int argc, char *argv[]){
 
   int sock;
@@ -41,15 +51,17 @@ int main(int argc, char *argv[]){
 	 sizeof(broadcastAddr)) < 0)
     throwError("bind() failed");
 
+
+  for(;;){
   if((recvStringLen = recvfrom(sock, recvString, 20,
 	 0, (struct sockaddr*)&msgSender, &msgLen)) < 0)
     throwError("recvfrom() failed");
 
   recvString[recvStringLen] = '\0';
-  printf("Recieved: %s from %d\n",
-	recvString, ntohl(msgSender.sin_addr.s_addr));
+  printf("Recieved: %s from ", recvString);
+  print_ip(ntohl(msgSender.sin_addr.s_addr));
+  }
 
   close(sock);
   exit(0);
-
 }
