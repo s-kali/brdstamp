ffc155d1b5a46da60e2860754835c0c48eb35286
Süha KÜÇÜKALİ <suhakucukali@marun.edu.tr>
2022-08-31 03:30:29 -0400

    recv msg from broadcast

--- receiver.c
+++ receiver.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <uinstd.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
@@ -33,8 +33,16 @@ int main(int argc, char *argv[]){
   broadcastAddr.sin_addr.s_addr = htonl(INADDR_ANY);
   broadcastAddr.sin_port = htons(broadcastPort);
 
-  if(bind(sock, (struct sockaddr *)&broadcastAddr), sizeof(broadcastAddr)) < 0)
+  if(bind(sock, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr)) < 0)
     throwError("bind() failed");
 
+  if((recvStringLen = recvfrom(sock, recvString, 20, 0, NULL, 0)) < 0)
+    throwError("recvfrom() failed");
+
+  recvString[recvStringLen] = '\0';
+  printf("Recieved: %s\n", recvString);
+
+  close(sock);
+  exit(0);
 
 }
