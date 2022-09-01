49b52c8e4f16d1b8a1cbccc992dc14368aab57e8
Süha KÜÇÜKALİ <suhakucukali@marun.edu.tr>
2022-09-01 07:37:15 -0400

    file structure

--- receiver.py
+++ receiver.py
@@ -1,10 +1,19 @@
 import socket
+import time
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock = socket.socket(
+	socket.AF_INET,
+	socket.SOCK_DGRAM,
+	socket.IPPROTO_UDP
+	)
+sock.setsockopt(
+	socket.SOL_SOCKET,
+	socket.SO_BROADCAST,
+	1
+	)
 
 sock.bind(("", 5555))
 
 while True:
 	data, addr = sock.recvfrom(1024)
-	print("received ", data.decode(), " from ", addr[0])
+	print("Timestamp on", addr[0], "is", data.decode(), "time diff =", int(time.time())-int(data.decode()))
--- sender.py
+++ sender.py
@@ -1,11 +1,26 @@
 import socket
 import time
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+sock = socket.socket(
+	socket.AF_INET,
+	socket.SOCK_DGRAM,
+	socket.IPPROTO_UDP
+	)
+
+sock.setsockopt(
+	socket.SOL_SOCKET,
+	socket.SO_BROADCAST,
+	1
+	)
 
 
 while True:
 	timestamp = str(int(time.time())).encode()
-	sock.sendto(timestamp, ("localhost", 5555))
+
+	sock.sendto(
+		timestamp,
+		("255.255.255.255", 5555)
+		)
+
 	time.sleep(3)
