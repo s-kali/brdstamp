b63f0680ef5c2e0f91f9326a13d7b795b9c07fec
Süha KÜÇÜKALİ <suhakucukali@marun.edu.tr>
2022-09-01 04:12:13 -0400

    broadcast timestamp

--- sender.py
+++ sender.py
@@ -4,8 +4,8 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-msg = b"first message"
 
 while True:
-	sock.sendto(msg, ("localhost", 5555))
+	timestamp = str(int(time.time())).encode()
+	sock.sendto(timestamp, ("localhost", 5555))
 	time.sleep(3)
