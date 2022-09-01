2e0bc8ae13af834af2b063876a433cdb7077a380
Süha KÜÇÜKALİ <suhakucukali@marun.edu.tr>
2022-09-01 04:03:19 -0400

    typeErr fix

--- sender.py
+++ sender.py
@@ -4,7 +4,7 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-msg = "first message"
+msg = b"first message"
 
 while True:
 	sock.sendto(msg, ("localhost", 5555))
